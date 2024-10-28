caaf11e1e3917f9e816f8ca1576241212c3af8f9
chenyuanjin <907815971@qq.com>
2024-10-28 20:26:40 +0800

    配置

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/Users/chenyuanjin/Desktop/project/blog/docs/.vuepress/.temp/pages/index.html.vue"
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1730118400000,\"contributors\":[{\"name\":\"chenyuanjin\",\"email\":\"907815971@qq.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
